830f10be660c10654db6b6b9ca3869c89ef3be4c
Asma875 <afsanapathan99@gmail.com>
2024-10-31 00:25:51 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example 
+
+
+let imgBox =document.getElementById("imgBox");
+let qrImg =document.getElementById("qrImg");
+let qrTextt =document.getElementById("qrText");
+
+function generateQR(){
+    if(qrText.value.length>0){
+        qrImg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }
+    else{
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+        },1000);
+    }
+
+
+}
